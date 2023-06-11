71ad932ed79736927e9ac7f1aed7214293a878d1
testgithubtiwari <rt936649@gmail.com>
2023-06-11 11:45:56 +0530

    some backend part of settings has been done

--- app/src/main/java/com/example/whatsapp/SettingsActivity.java
+++ app/src/main/java/com/example/whatsapp/SettingsActivity.java
@@ -1,15 +1,26 @@
 package com.example.whatsapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.whatsapp.databinding.ActivityLoginBinding;
 import com.example.whatsapp.databinding.ActivitySettingsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class SettingsActivity extends AppCompatActivity {
 
     ActivitySettingsBinding binding;
+
+    FirebaseAuth mauth;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
 
 
     @Override
@@ -20,5 +31,42 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
+
+        mauth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+        storage=FirebaseStorage.getInstance();
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                        Intent.FLAG_ACTIVITY_NEW_TASK);
+            }
+        });
+
+        binding.plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/");
+                startActivityForResult(intent,25);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(data.getData()!=null)
+        {
+            Uri sfile=data.getData();
+            binding.profileImage.setImageURI(sfile);
+
+        }
     }
 }
