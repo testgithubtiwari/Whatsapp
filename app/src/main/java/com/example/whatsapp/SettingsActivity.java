b35bbe83f518e020e125b0ea5451310ad0bf96df
testgithubtiwari <rt936649@gmail.com>
2023-06-11 16:17:26 +0530

    some backend part of settings has been done

--- app/src/main/java/com/example/whatsapp/SettingsActivity.java
+++ app/src/main/java/com/example/whatsapp/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,12 +8,23 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.whatsapp.Models.users;
 import com.example.whatsapp.databinding.ActivityLoginBinding;
 import com.example.whatsapp.databinding.ActivitySettingsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -56,6 +68,49 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        binding.savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!binding.txtusername.getText().toString().equals("") && !binding.etAbout.getText().toString().equals(""))
+                {
+                    String status=binding.etAbout.getText().toString();
+                    String username=binding.txtusername.getText().toString();
+
+                    HashMap<String,Object> obj=new HashMap<>();
+                    obj.put("username",username);
+                    obj.put("About",status);
+
+                    database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+                            .updateChildren(obj);
+
+                    Toast.makeText(SettingsActivity.this, "Profile Updated", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(SettingsActivity.this, "Please fill the details!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        users User=snapshot.getValue(users.class);
+                        Picasso.get()
+                                .load(User.getProfilepic())
+                                .placeholder(R.drawable.avatar).into(binding.profileImage);
+
+                        binding.etAbout.setText(User.getStatus());
+                        binding.txtusername.setText(User.getUsername());
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(SettingsActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 
     @Override
@@ -66,7 +121,21 @@ public class SettingsActivity extends AppCompatActivity {
         {
             Uri sfile=data.getData();
             binding.profileImage.setImageURI(sfile);
+            final StorageReference reference= storage.getReference().child("profile_pic")
+                .child(FirebaseAuth.getInstance().getUid());
 
+            reference.putFile(sfile).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid()).
+                                    child("Profile_pic").setValue(uri.toString());
+                        }
+                    });
+                }
+            });
         }
     }
 }
